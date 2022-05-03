e355107c741da60d086c053f87b696b797c88e3c
Rupasappati <rupasappati@gmail.com>
2022-05-03 18:42:47 +0530

    Addition funtionality of calculator

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace calculator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator objCalc = new calculator();
+            Console.WriteLine("enter 1st number = ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter 2nd number = ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            objCalc.Addition(num1, num2);
+
+        }
+    }
+
+}
